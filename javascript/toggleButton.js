f10b74398f92c4506e2bce82a12e56d116b2ef63
zarifshawon <zarifshawon001@gmail.com>
2021-02-20 16:13:16 +0600

    toggolebutton

--- javascript/toggleButton.js
+++ javascript/toggleButton.js
@@ -1,8 +1,14 @@
 function toggleButton() {
   var x = document.getElementById("doctorAcc");
+  var y = document.getElementById("doctorAcc1");
+
   if (x.style.display === "none") {
     x.style.display = "block";
+    y.style.display = "none";
   } else {
     x.style.display = "none";
+    y.style.display = "block";
   }
+
+
 }
