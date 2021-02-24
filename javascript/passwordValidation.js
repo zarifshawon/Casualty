c684ce30f3d65de7970c19bba9da2722bafa0a74
zarifshawon <zarifshawon001@gmail.com>
2021-02-24 14:06:10 +0600

    password validation js

--- javascript/passwordValidation.js
+++ javascript/passwordValidation.js
@@ -1,9 +1,14 @@
-function passwordValidation() {
-    var password = document.getElementById("password-verify").value;
-    var confirmPassword = document.getElementById("password-verify2").value;
-    if (password != confirmPassword) {
-        alert("Passwords do not match.");
-        return false;
-    }
-    return true;
+
+var password = document.getElementById("password-verify")
+  , confirm_password = document.getElementById("password-verify2");
+
+function passwordValidation(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
 }
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
